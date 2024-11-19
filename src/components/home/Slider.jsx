670540f858010fef6b52cc0e4f77afbd0c99d425
Faizan Ali Khan <k.faizan@i-diligence.com>
2024-11-19 17:55:50 +0530

    Slider Complete

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -1,14 +1,20 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+import { Card, Typography } from "@material-tailwind/react";
 const Slider = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
   return (
     <div>
         <Swiper
+        onSlideChange={(swiper)=>{
+            setActiveIndex(swiper.activeIndex)
+          }}
         spaceBetween={30}
         effect={"fade"}
         centeredSlides={true}
@@ -16,20 +22,53 @@ const Slider = () => {
           delay: 5000,
           disableOnInteraction: false,
         }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[EffectFade, Pagination, Autoplay]}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        modules={[EffectFade, Autoplay]}
         className="mySwiper h-full"
       >
         <SwiperSlide>
-            <div className="w-full bg-no-repeat bg-cover bg-center h-[548px]" style={{backgroundImage: 'url("/banner.jpg")'}}>
-                <img src="/slide1.jpeg" alt="" className="w-full"/>
+            <div className="relative w-full bg-no-repeat bg-cover bg-center h-[548px]" style={{backgroundImage: 'url("/banner.jpg")'}}>
+        {activeIndex == 0 &&<motion.div
+          className="w-full h-full relative"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+                <Card className="absolute bottom-40 max-w-lg right-6 bg-[#02a458]/50 backdrop-blur-sm p-4">
+                <Typography
+                  variant="h1"
+                  className={`text-3xl md:text-4xl  text-white`}
+                >
+                  Your Journey Towards Wellness Starts Here
+                  {/* <span className="tracking-wide ml-2"></span> */}
+                </Typography>
+                </Card>
+        </motion.div>
+        }
             </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="w-full bg-no-repeat bg-cover bg-center h-[548px]" style={{backgroundImage: 'url("/banner2.jpg")'}}>
-                <img src="/slide2.jpg" alt="" className="w-full"/>
+          <div className="relative w-full bg-no-repeat bg-cover bg-center h-[548px]" style={{backgroundImage: 'url("/banner2.jpg")'}}>
+          {activeIndex == 1 &&
+          <motion.div
+          className="w-full relative h-full"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Card className="absolute bottom-40 max-w-lg right-6 bg-[#02a458]/50 backdrop-blur-sm p-4">
+                <Typography
+                  variant="h1"
+                  className={`text-3xl md:text-4xl  text-white`}
+                >
+                  Building a Bridge to Wellness
+                  {/* <span className="tracking-wider ml-2">to Wellness</span> */}
+                </Typography>
+                </Card>
+            </motion.div>
+            }
             </div>
           </SwiperSlide>
       </Swiper>
